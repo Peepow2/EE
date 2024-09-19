156c6bc6db39d46f057eff136877b3396e761c55
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-09-20 03:20:19 +0700

    Create LAB2_Cir2_(214).m

---
+++ LAB2_Cir2_(214).m
@@ -0,0 +1,30 @@
+%% set font (latex)
+Lw = 8; Fsize = 24;
+
+%% Section 1
+Filename = "Cir2Lab1.csv"; %"Lab2_1.csv";
+DATA = readmatrix(Filename);
+time  = DATA(:, 1) * 1000;
+Vs = DATA(:, 2);
+Vc = DATA(:, 3);
+
+plot(time, [Vs, Vc], LineWidth = Lw)
+xlim([0 10])
+legend("$V_{s}$","$V_{c}$", 'Interpreter', 'latex',FontSize = Fsize)
+xlabel('$time (ms)$', 'Interpreter', 'latex',FontSize = Fsize)
+ylabel('$Voltage (V)$', 'Interpreter', 'latex',FontSize = Fsize)
+grid on
+
+%% Section 2
+Filename = "Cir2Lab1.csv"; %"Lab2_1.csv";
+DATA = readmatrix(Filename);
+time  = DATA(:, 1) * 1000;
+Vs = DATA(:, 2);
+Vc = DATA(:, 3);
+
+plot(time, [Vs, Vc], LineWidth = Lw)
+xlim([0 10])
+legend("$V_{s}$","$V_{c}$", FontSize = Fsize)
+xlabel('$time (ms)$', FontSize = Fsize)
+ylabel('$Voltage (V)$', FontSize = Fsize)
+grid on
