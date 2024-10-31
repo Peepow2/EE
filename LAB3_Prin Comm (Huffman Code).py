65105b5f1066a8dcc19cff2b9123e61e6b5810e1
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-11-01 01:36:16 +0700

    Update LAB3_Prin Comm (Huffman Code).py

--- LAB3_Prin Comm (Huffman Code).py
+++ LAB3_Prin Comm (Huffman Code).py
@@ -1,21 +1,22 @@
 # เตรียมข้อมูล สร้างอาเรย์ (ตัวอักษร, ความน่าจะเป็น)
-def preparation(S): 
-    char_freq = [(c, NAME.count(c)/len(S)) for c in sorted(set(S))]
+def Data_Preparation(S): 
+    char_freq = sorted([(c, NAME.count(c)/len(S)) for c in set(S)], key = lambda x:x[1])
     # print(char_freq)
     return char_freq
 
 def show_result(Huffman_Code, Binary_code):
     print(Binary_code)
     print(f"Huffman_Code = {Huffman_Code} and length = {len(Huffman_Code)}")
-    return 0
+    return
 
 
 # เข้ารหัส Huffman_Code
 def Huffman_Encoding(NAME):
+    Data_Preparation(NAME)
     return "", []
 
 
 # Driver Code (MAIN)
-NAME = ""
+NAME = "peerawichsodsuay"
 Huffman_Code, Binary_code = Huffman_Encoding(NAME)
 show_result(Huffman_Code, Binary_code) # แสดงผลลัพธ์
