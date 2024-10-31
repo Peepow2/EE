fa59b021aeab1ae7ccc3350de79a4f098616e519
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-11-01 04:42:01 +0700

    Update LAB3_Prin Comm (Huffman Code).py

--- LAB3_Prin Comm (Huffman Code).py
+++ LAB3_Prin Comm (Huffman Code).py
@@ -12,7 +12,6 @@ def show_result(Huffman_Code, Binary_code):
     print(f"Huffman_Code = {Huffman_Code} and length = {len(Huffman_Code)}")
     return
 
-
 def upper_bound(val, Arr):
     for i in range(len(Arr)-1, 0, -1):
         if Arr[i] >= val:
@@ -51,7 +50,6 @@ def Huffman_Encoding(NAME):
         Huffman_Code += Code[0][char.index(c)]
 
     return Huffman_Code, Binary_code
-
 
 # Driver Code (MAIN)
 NAME = "peerawichsodsuay" #"A" * 30 + "B" * 25 + "C" * 20 + "D" * 12 + "E" * 8 + "F" * 5
