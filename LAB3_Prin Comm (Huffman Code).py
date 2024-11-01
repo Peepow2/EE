0edc66d097ae0f5309392814bff6bdfab3d857b7
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-11-01 07:14:13 +0700

    Update LAB3_Prin Comm (Huffman Code).py

--- LAB3_Prin Comm (Huffman Code).py
+++ LAB3_Prin Comm (Huffman Code).py
@@ -1,7 +1,7 @@
 # เตรียมข้อมูล สร้างอาเรย์ (ตัวอักษร, ความน่าจะเป็น)
 def Data_Preparation(S): 
     char_freq = sorted([(c, NAME.count(c)/len(S)) for c in set(S)], \
-                       key = lambda x:x[1], reverse = True)
+                       key = lambda x: (x[1], x[0]), reverse = True)
     # print(char_freq)
     char = [itr[0] for itr in char_freq]
     freq = [itr[1] for itr in char_freq]
@@ -21,33 +21,12 @@ def upper_bound(val, Arr):
 # เข้ารหัส Huffman_Code
 def Huffman_Encoding(NAME):
     char_freq, char, freq = Data_Preparation(NAME)
-    Code = [[""] * (len(char)-i) for i in range(len(char)-1)]
 
-    Node = list([freq])
-    for i in range(2, len(char)):
-        val = sum(Node[-1][-2::])
-        N = list(Node[-1][:-2:])
-        idx = upper_bound(val, N)
-        N.insert(idx, val)
-        Node.append(N)
-    
-    Code[-1] = ["0", "1"]
-    for i in range(len(Node) - 1, 0, -1):
-        for j in range(len(Node[i]) - 1, -1, -1):
-            if Node[i][j] == Node[i-1][-2] + Node[i-1][-1]:
-                Code[i-1][-2] += Code[i][j] + "0"
-                Code[i-1][-1] += Code[i][j] + "1"
-                break
-
-        for j in range(len(Node[i-1])-2):
-            for k in range(len(Node[i])):
-                if Node[i-1][j] == Node[i][k] and Code[i-1][j] == "":
-                    Code[i-1][j] += Code[i][k]
-
-    Binary_code = [(char[i], Code[0][i]) for i in range(len(char))]
+    Binary_code = list()
     Huffman_Code = ""
-    for c in NAME: Huffman_Code += Code[0][char.index(c)]
+
     return Huffman_Code, Binary_code
+
 
 # Driver Code (MAIN)
 NAME = "peerawichsodsuay" #"A" * 30 + "B" * 25 + "C" * 20 + "D" * 12 + "E" * 8 + "F" * 5
