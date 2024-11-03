9b1ffe5d201de009f7959e3ac33ea60a132b6f37
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2024-11-03 14:57:34 +0700

    Update LAB3_Prin Comm (Huffman Code).py

--- LAB3_Prin Comm (Huffman Code).py
+++ LAB3_Prin Comm (Huffman Code).py
@@ -1,4 +1,3 @@
-# เตรียมข้อมูล สร้างอาเรย์ (ตัวอักษร, ความน่าจะเป็น)
 def Data_Preparation(S): 
     char_freq = sorted([(c, NAME.count(c)/len(S)) for c in set(S)], \
                        key = lambda x: (x[1], x[0]), reverse = True)
@@ -7,10 +6,13 @@ def Data_Preparation(S):
     freq = [itr[1] for itr in char_freq]
     return char_freq, char, freq
 
+
 def show_result(Huffman_Code, Binary_code):
-    print(Binary_code)
-    print(f"Huffman_Code = {Huffman_Code} and length = {len(Huffman_Code)}")
+    print(f"Binary_code = {Binary_code}")
+    print(f"Huffman_Code = {Huffman_Code}")
+    print(f"length = {len(Huffman_Code)}")
     return
+
 
 def upper_bound(val, Arr):
     for i in range(len(Arr)-1, 0, -1):
@@ -18,17 +20,41 @@ def upper_bound(val, Arr):
             return i + 1
     return 0
 
-# เข้ารหัส Huffman_Code
+
 def Huffman_Encoding(NAME):
     char_freq, char, freq = Data_Preparation(NAME)
+    P_xi = [freq]
+    Code = [['0', '1']]
+    
 
+    for i in range(1, len(char) - 1):
+        val = P_xi[i-1][-2] + P_xi[i-1][-1]
+        idx = upper_bound(val, P_xi[i-1][:-2:])
+        P_xi.append(P_xi[i-1][:idx] + [val] + P_xi[i-1][idx:-2])
+
+
+    for i in range(len(P_xi) - 1, 0, -1):
+        least = P_xi[i-1][-2] + P_xi[i-1][-1]
+        for j in range(len(P_xi[i]) - 1, -1, -1):
+            if P_xi[i][j] == least:
+                C1 = Code[0][j] + '0'
+                C2 = Code[0][j] + '1'
+                Code.insert(0, Code[0][:j] + Code[0][j+1:] + [C1, C2])
+                break
+    
     Binary_code = list()
     Huffman_Code = ""
+
+    for i in range(len(char)):
+        Binary_code.append((char[i], Code[0][i]))
+
+    for c in NAME: 
+        Huffman_Code += Code[0][char.index(c)]
 
     return Huffman_Code, Binary_code
 
 
 # Driver Code (MAIN)
-NAME = "peerawichsodsuay" #"A" * 30 + "B" * 25 + "C" * 20 + "D" * 12 + "E" * 8 + "F" * 5
+NAME = "peerawichsodsuay" #input("Your Name: ")
 Huffman_Code, Binary_code = Huffman_Encoding(NAME)
-show_result(Huffman_Code, Binary_code) # แสดงผลลัพธ์
+show_result(Huffman_Code, Binary_code)
