3cd12f0c86da3c3b6939fdc1e32be55a1bda68e2
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2025-01-15 01:06:17 +0700

    Update Homework 1.cpp

--- 2102444_Embed sys/Homework 1.cpp
+++ 2102444_Embed sys/Homework 1.cpp
@@ -4,7 +4,7 @@ int main()
     std::string name = "Peerawich Sodsuay";
     std::cout << "Hello world! My name is " << name << ".";
     return 0;
-} "https://onlinegdb.com/saH_4A5oB"
+} /* https://onlinegdb.com/saH_4A5oB */
 
 // ---------------------------------------------------------- //
 int LED_pin = 5;
@@ -19,5 +19,5 @@ void loop()
   delay(1000);
   digitalWrite(LED_pin, LOW);
   delay(1000);
-} "https://wokwi.com/projects/420079422533783553"
+} /* "https://wokwi.com/projects/420079422533783553 */
 
