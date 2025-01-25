37c8a1debaff07b5741c229b7e546afc1f1fac26
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2025-01-26 01:27:29 +0700

    Update HW3 Func.m

--- 2102513-14/HW3 Func.m
+++ 2102513-14/HW3 Func.m
@@ -1,6 +1,12 @@
 function y = f(x, A, B)
     a = A(1); b = A(2); 
     c = B(1); d = B(2);
+    %{ % Alternative
+    y = x
+    result(y >= 0 & y < a)  = (c/a) * result(y >= 0 & y < a);
+    result(y >= a & y < b)  = ((d-c)/(b-a)) * result(y >= a & y < b)  + (c - (((d-c)/(b-a))*a));
+    result(y >= b & y <= 1) = ((1-d)/(1-b)) * result(y >= b & y <= 1) + (d - (((1-d)/(1-b))*b));
+    }%
     
     % For 0 <= x < a (First interval)
     if (x >= 0) && (x < a)
@@ -11,7 +17,7 @@ function y = f(x, A, B)
         y = ((d-c)/(b-a))*x + c - (((d-c)/(b-a))*a);
     
     % For b <= x < 1 (Third interval)
-    elseif (x >= b) && (x < 1)
+    elseif (x >= b) && (x <= 1)
         y = ((1-d)/(1-b))*x + d - (((1-d)/(1-b))*b);
     
     % Another case
