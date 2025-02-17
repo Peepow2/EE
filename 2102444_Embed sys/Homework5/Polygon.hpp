37342fac2b5a8a7225b79330710bfc04c2082853
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2025-02-17 22:31:19 +0700

    Update Polygon.hpp

--- 2102444_Embed sys/Homework5/Polygon.hpp
+++ 2102444_Embed sys/Homework5/Polygon.hpp
@@ -2,28 +2,35 @@
 
 #include "Point.h"
 #include <vector> 
-namespace mySpace {
+
+namespace mySpace 
+{
     using std::vector;
     using std::cout;
     using Points = vector<Point>;
     
     // Polygon with a vector of N_ Points
-    class Polygon {
+    class Polygon 
+    {
         private:
-            Points P_; // Main Attribute
-            int N_; // Auxillary Attribute
+            Points P_;    // Main Attribute
+            int N_;       // Auxillary Attribute
         public:
             // default constructor
             Polygon () {N_=0;}
+
             // constructor with a vector of N points
             Polygon (Points P) : P_(P) {N_=P.size();}
             
             // length from P_[i] to P_[i+1]
-            float length(int i=0) {
+            float length(int i=0) 
+            {
                 // check if valid value of i and N
-                if ((i>=0) && (i < N_) && (N_>1)) {
+                if ((i>=0) && (i < N_) && (N_>1)) 
+                {
                     return P_[i].length(nextPoint(i));
                 }
+                    
                 else // otherwise return 0
                     return 0;
             }
@@ -32,16 +39,20 @@ namespace mySpace {
             void print() 
             {
                 cout << "A Polygon with " << N_ << " points.\n"; 
+                
                 // traditional for loop
                 // int i and j is only local to the for loop
                 for (int i=0; i<N_ ; i++) 
                 {
                     cout << "P" <<i << " is @"; P_[i].print(); cout<<".\n";
                 }
+                
                 // another int i but available outside while loop (inside print)
                 int i=0;
+                
                 // while loop
-                while (i<N_) {
+                while (i<N_) 
+                {
                     // j is local to the while loop
                     cout << "Line P"<<i << "-P" << nextIndex(i)<<" is "<< length(i) << " unit long.\n";
                     i++;
@@ -50,13 +61,17 @@ namespace mySpace {
                 cout << "Its area is " << area() << " unit2.\n";
                 cout << "\n";
             }
-            float perimeter() {
+
+            float perimeter() 
+            {
                 float sum = 0;
                 for (int i=0; i<N_; i++)
                     sum += length(i);
                 return sum;
             }
-            float area() { 
+
+            float area() 
+            { 
                 float Area {0}; 
                 float up = 0, down = 0;
                 
@@ -70,6 +85,7 @@ namespace mySpace {
                 Area = std::abs(up-down) * 0.5;
                 return Area;
             }
+
             void moveX(float X) 
             {
                 moveXY(X);            
@@ -106,11 +122,16 @@ namespace mySpace {
                 }
                 moveXY(-x1,-y1);
             }
-            Point nextPoint(int i) {
+
+            Point nextPoint(int i) 
+            {
                 return P_[ nextIndex(i) ];
+                
             }
+
             // next Index is i+1 if i+1 is not N_ otherwise zero.
-            int nextIndex(int i) {
+            int nextIndex(int i) 
+            {
                 return (i+1) % N_;
             }
     };
