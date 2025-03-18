6a7c491498b4feb3220c9a4408a71566cd2be38a
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2025-03-18 14:01:26 +0700

    Create Class_Code.cpp

---
+++ 2102444_Embed sys/Homework_7/Class_Code.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class 444_member
+{
+    private:
+    string ID, Name;
+    int Age;
+    
+    public:
+    
+}
+
+int main()
+{
+    return 0;
+}
