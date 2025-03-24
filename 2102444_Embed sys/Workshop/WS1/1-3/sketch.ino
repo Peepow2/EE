9782b9258312b4ee2050a5efe4a27fc9393e597c
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2025-03-24 17:23:04 +0700

    Update sketch.ino

--- 2102444_Embed sys/Workshop/WS1/1-3/sketch.ino
+++ 2102444_Embed sys/Workshop/WS1/1-3/sketch.ino
@@ -1,1 +1,28 @@
+const int LDR_PIN = A2;  // Analog pin for LDR
+const int LED_PIN = 7;   // Analog pin for LED
+const int ButtonPin = 2; // Digital pin for Button
 
+bool state = true;
+int LED_val = 0;
+
+void setup() 
+{
+  Serial.begin(9600);  
+  pinMode(ButtonPin, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() 
+{
+  int ldrValue = analogRead(LDR_PIN); // Read ldrValue
+
+  if (digitalRead(ButtonPin) == HIGH)
+    state = !state;
+
+  if (state)
+    LED_val = map(ldrValue, 200, 700, 0, 255);
+  else
+    LED_val = map(ldrValue, 700, 200, 0, 255);
+
+  analogWrite(LED_PIN, LED_val);
+}
