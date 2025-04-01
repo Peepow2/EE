de91f921f2b33e40f69462b10fa018f6c42ddef3
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2025-04-01 13:51:13 +0700

    Update 1.ino

--- 2102444_Embed sys/QUIZ/1.ino
+++ 2102444_Embed sys/QUIZ/1.ino
@@ -1,16 +1,16 @@
-int Buzzer = 2;
-int Sensor = 3;
+int Buzzer = 2; // Set Buzzer in D2 PIN
+int Sensor = 3; // Set Sensor in D3 PIN
 
 void setup() 
 {
-  pinMode(Sensor, INPUT);
-  pinMode(Buzzer, OUTPUT);
+  pinMode(Sensor, INPUT);  // Sensor PIN as INPUT
+  pinMode(Buzzer, OUTPUT); // Buzzer PIN as OUTPUT
 }
 
 void loop() 
 {
-  int val = digitalRead(Sensor);
-  if (val == 1)
+  int val = digitalRead(Sensor); // Read the value from Sensor
+  if (val == 1) // if found the vibration alert for 1 sec
   {
     digitalWrite(Buzzer, HIGH);
     delay(1000);
